cd07f9cb2ff5ba544ab27b7a9f3e60949b44fd30
pedrovb <pedrovictor_caldas@hotmail.com>
2018-08-07 13:37:03 -0300

    Add basic operations

--- esm/Connector.js
+++ esm/Connector.js
@@ -1,11 +1,28 @@
+const ioc = require('socket.io-client');
+
 class Connector {
-  async start() { // eslint-disable-line no-empty-function
+  async start(host, port) { // eslint-disable-line no-empty-function
+    ioc.connect(`${host}:${port}/`);
+
+    ioc.on('onConfigUpdated', (data) => {
+      console.log(data);
+    });
+
+    ioc.on('onPropertiesUpdated', (data) => {
+      console.log(data);
+    });
+
+    ioc.on('onDataRequested', (data) => {
+      console.log(data);
+    });
   }
 
   async addDevice(device) { // eslint-disable-line no-empty-function,no-unused-vars
+    ioc.emit('addDevice', device);
   }
 
   async removeDevice(id) { // eslint-disable-line no-empty-function,no-unused-vars
+    ioc.emit('removeDevice', id);
   }
 
   async listDevices() { // eslint-disable-line no-empty-function,no-unused-vars
@@ -14,30 +31,45 @@ class Connector {
   // Device (fog) to cloud
 
   async publishData(id, data) { // eslint-disable-line no-empty-function,no-unused-vars
+    ioc.emit('publishData', id, data);
   }
 
   async updateSchema(id, schema) { // eslint-disable-line no-empty-function,no-unused-vars
+    ioc.emit('updateSchema', id, schema);
   }
 
   async updateProperties(id, properties) { // eslint-disable-line no-empty-function,no-unused-vars
+    ioc.emit('updateProperties', id, properties);
   }
 
   // Cloud to device (fog)
 
   // cb(event) where event is { id, config: {} }
   onConfigUpdated(cb) { // eslint-disable-line no-empty-function,no-unused-vars
+    ioc.on('onConfigUpdated', (data) => {
+      cb(data);
+    });
   }
 
   // cb(event) where event is { id, properties: {} }
   onPropertiesUpdated(cb) { // eslint-disable-line no-empty-function,no-unused-vars
+    ioc.on('onPropertiesUpdated', (data) => {
+      cb(data);
+    });
   }
 
   // cb(event) where event is { id, sensorId }
   onDataRequested(cb) { // eslint-disable-line no-empty-function,no-unused-vars
+    ioc.on('onDataRequested', (data) => {
+      cb(data);
+    });
   }
 
   // cb(event) where event is { id, sensorId, data }
   onDataUpdated(cb) { // eslint-disable-line no-empty-function,no-unused-vars
+    ioc.on('onDataUpdated', (data) => {
+      cb(data);
+    });
   }
 }
 
