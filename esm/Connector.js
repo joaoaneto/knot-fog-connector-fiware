0b4cf700e0ec01cf3eb89e915ebc4bb352f477eb
João Neto <joao.alexandre@cesar.org.br>
2018-09-03 22:48:43 -0300

    Initial MQTT implementation

--- esm/Connector.js
+++ esm/Connector.js
@@ -1,43 +1,118 @@
+import request from 'request-promise-native';
+import mqtt from 'mqtt';
+
+const headers = {
+  'fiware-service': 'knot',
+  'fiware-servicepath': '/'
+}
+
+const apiKey = 'knot123';
+
+function mapDeviceToFiware(device) {
+  return {
+    devices: [{
+      device_id: device.id,
+      entity_name: device.name,
+      entity_type: 'device',
+      protocol:'IoTA-UL',
+      transport: 'MQTT',
+      attributes: [{
+          object_id: 'o',
+          name: 'online',
+          type: 'Boolean'
+      }]
+    }]
+  }
+}
+
+function mapSensorToFiware(id, schema) {
+  return {
+    devices: [{
+      device_id: schema[0].sensor_id,
+      entity_name: schema[0].name,
+      entity_type: 'sensor',
+      protocol:'IoTA-UL',
+      transport: 'MQTT',
+      attributes: [{
+          object_id: 'o',
+          name: 'online',
+          type: 'Boolean'
+      }],
+      commands: [
+        {
+          name: 'setData',
+          type: 'command'
+        },
+        {
+          name: 'getData',
+          type: 'command'
+        }
+      ],
+      static_attributes: [{
+          name: 'device',
+          type: 'string',
+          value: id
+      }]
+    }]
+  }
+}
+
 class Connector {
-  async start() { // eslint-disable-line no-empty-function
+  async start() {
+
   }
 
-  async addDevice(device) { // eslint-disable-line no-empty-function,no-unused-vars
+  async addDevice(device) {
+    const url = '';
+    const fiwareDevice = mapDeviceToFiware(device);
+    await request.post({url, headers: headers, json: true}).form(fiwareDevice);
   }
 
-  async removeDevice(id) { // eslint-disable-line no-empty-function,no-unused-vars
+  async removeDevice(id) {
+    const url = `http://localhost:4041/iot/devices/${id}`;
+    await request.delete({url, headers: headers});
   }
 
-  async listDevices() { // eslint-disable-line no-empty-function,no-unused-vars
+  async listDevices() {
+    const url = 'http://localhost:4041/iot/devices';
+    return request.get({url, headers:headers});
   }
 
   // Device (fog) to cloud
 
-  async publishData(id, data) { // eslint-disable-line no-empty-function,no-unused-vars
+  async publishData(id, data) {
+    this.iota.publish(`/${apiKey}/${data.sensor_id}/attrs/${data.sensor_id}`, data.value);
   }
 
-  async updateSchema(id, schema) { // eslint-disable-line no-empty-function,no-unused-vars
+  async updateSchema(id, schema) {
+    schema.forEach((schema) => {
+      const sensorFiware = mapSensorToFiware(id, schema);
+      await request.post({url, headers: headers, json: true}).form(sensorFiware);
+    });
   }
 
-  async updateProperties(id, properties) { // eslint-disable-line no-empty-function,no-unused-vars
+  async updateProperties(id, properties) {
   }
 
   // Cloud to device (fog)
 
   // cb(event) where event is { id, config: {} }
-  onConfigUpdated(cb) { // eslint-disable-line no-empty-function,no-unused-vars
+  onConfigUpdated(cb) {
   }
 
   // cb(event) where event is { id, properties: {} }
-  onPropertiesUpdated(cb) { // eslint-disable-line no-empty-function,no-unused-vars
+  onPropertiesUpdated(cb) {
   }
 
   // cb(event) where event is { id, sensorId }
-  onDataRequested(cb) { // eslint-disable-line no-empty-function,no-unused-vars
+  onDataRequested(cb) {
+    // subscribe to listen getData command
+    this.iota.subscribe(`/${apiKey}/#/cmd`);
   }
 
   // cb(event) where event is { id, sensorId, data }
-  onDataUpdated(cb) { // eslint-disable-line no-empty-function,no-unused-vars
+  onDataUpdated(cb) {
+    // subscribe to listen setData command
   }
 }
 
