dbe7881f7086feeb4bd7dfead0e949f75566e1cc
João Neto <joao.alexandre@cesar.org.br>
2018-09-06 11:28:59 -0300

    Create IoT Agent service at start
    Change-Id: I49e1f8d1673487fdb844ccc58f1ef3758bf6074c

--- esm/Connector.js
+++ esm/Connector.js
@@ -1,5 +1,66 @@
+import request from 'request-promise-native';
+
+async function serviceExists(url, headers) {
+  const service = await request.get({ url, headers, json: true });
+  return service.count > 0;
+}
+
+async function createService(iotAgentUrl, serviceConfig, servicePath, apiKey, entityType) {
+  const url = `${iotAgentUrl}/iot/services`;
+  const service = serviceConfig;
+  const headers = {
+    'fiware-service': service.name,
+    'fiware-servicepath': servicePath,
+  };
+
+  if (await serviceExists(url, headers)) {
+    return;
+  }
+
+  service.entity_type = entityType;
+  service.apikey = apiKey;
+
+  if (entityType === 'device') {
+    service.commands = [
+      {
+        name: 'setConfig',
+        type: 'command',
+      },
+      {
+        name: 'setProperties',
+        type: 'command',
+      },
+    ];
+  } else if (entityType === 'sensor') {
+    service.attributes = [{
+      name: 'value',
+      type: 'string',
+    }];
+    service.commands = [
+      {
+        name: 'setData',
+        type: 'command',
+      },
+      {
+        name: 'getData',
+        type: 'command',
+      },
+    ];
+  }
+
+  await request.post({
+    url, headers, body: { services: [service] }, json: true,
+  });
+}
+
 class Connector {
-  async start() { // eslint-disable-line no-empty-function
+  constructor(settings) {
+    this.serviceConfig = settings.service;
+    this.iotAgentUrl = `http://${settings.hostname}:${settings.port}`;
+  }
+
+  async start() {
+    await createService(this.iotAgentUrl, this.serviceConfig, '/device', 'default', 'device');
   }
 
   async addDevice(device) { // eslint-disable-line no-empty-function,no-unused-vars
