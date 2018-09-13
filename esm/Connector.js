00e4c39ef697c0b48fe0b54c7a002350e04c1663
João Neto <joao.alexandre@cesar.org.br>
2018-09-13 16:04:21 -0300

    Add listen to updated config
    Change-Id: I0d9703ab7ce7dd6fc15b1cbc2623293a1faf3f60

--- esm/Connector.js
+++ esm/Connector.js
@@ -130,7 +130,6 @@ function parseULValue(value) {
   return objValue;
 }
 
-
 function parseULMessage(topic, message) {
   const apiKey = topic.split('/')[1];
   const entityId = message.slice(0, message.indexOf('@'));
@@ -159,6 +158,24 @@ async function messageHandler(topic, payload) {
     this.onDataRequestedCb(
       { id: message.id, sensorId: parseInt(message.entityId, 10) },
     );
+  } else if (message.command === 'setConfig') {
+    const requiredProperties = ['sensor_id', 'event_flags', 'time_sec'];
+    const configKeys = Object.keys(message.value);
+
+    if (!requiredProperties.every(val => configKeys.includes(val))) {
+      const response = 'The following properties are required: sensor_id, event_flags and time_sec';
+      await this.client.publish(`${topic}exe`, `${message.id}@setConfig|${response}`);
+      return;
+    }
+
+    _.forEach(message.value, (value, key) => {
+      const intValue = parseInt(value, 10);
+      message.value[key] = !Number.isNaN(intValue) && Number.isFinite(intValue) ? intValue : value;
+    });
+
+    await this.client.publish(`${topic}exe`, `${message.id}@setConfig|`);
+
+    this.onConfigUpdatedCb({ id: message.id, config: message.value });
   }
 }
 
@@ -173,6 +190,7 @@ class Connector {
   async start() {
     this.onDataUpdatedCb = _.noop();
     this.onDataRequestedCb = _.noop();
+    this.onConfigUpdatedCb = _.noop();
 
     await createService(this.iotAgentUrl, this.orionUrl, this.serviceConfig, '/device', 'default', 'device');
 
@@ -204,6 +222,7 @@ class Connector {
       url, headers, body: { devices: [fiwareDevice] }, json: true,
     });
     await createService(this.iotAgentUrl, this.orionUrl, this.serviceConfig, `/device/${device.id}`, device.id, 'sensor');
+    await this.client.subscribe(`/default/${device.id}/cmd`);
   }
 
   async removeDevice(id) { // eslint-disable-line no-empty-function,no-unused-vars
@@ -284,7 +303,8 @@ class Connector {
   // Cloud to device (fog)
 
   // cb(event) where event is { id, config: {} }
-  onConfigUpdated(cb) { // eslint-disable-line no-empty-function,no-unused-vars
+  async onConfigUpdated(cb) {
+    this.onConfigUpdatedCb = cb;
   }
 
   // cb(event) where event is { id, properties: {} }
