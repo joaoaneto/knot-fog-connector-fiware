6248a58103ee18d3d5f58f473e84999e8d1d824b
João Neto <joao.alexandre@cesar.org.br>
2018-09-14 10:43:16 -0300

    Add listen to updated properties
    Change-Id: Id8078e0549cd9e69f182a08889b6bd2979a58d5d

--- esm/Connector.js
+++ esm/Connector.js
@@ -176,6 +176,9 @@ async function messageHandler(topic, payload) {
     await this.client.publish(`${topic}exe`, `${message.id}@setConfig|`);
 
     this.onConfigUpdatedCb({ id: message.id, config: message.value });
+  } else if (message.command === 'setProperties') {
+    await this.client.publish(`${topic}exe`, `${message.id}@setProperties|`);
+    this.onPropertiesUpdatedCb({ id: message.id, properties: message.value });
   }
 }
 
@@ -191,6 +194,7 @@ class Connector {
     this.onDataUpdatedCb = _.noop();
     this.onDataRequestedCb = _.noop();
     this.onConfigUpdatedCb = _.noop();
+    this.onPropertiesUpdatedCb = _.noop();
 
     await createService(this.iotAgentUrl, this.orionUrl, this.serviceConfig, '/device', 'default', 'device');
 
@@ -308,7 +312,8 @@ class Connector {
   }
 
   // cb(event) where event is { id, properties: {} }
-  onPropertiesUpdated(cb) { // eslint-disable-line no-empty-function,no-unused-vars
+  async onPropertiesUpdated(cb) {
+    this.onPropertiesUpdatedCb = cb;
   }
 
   // cb(event) where event is { id, sensorId }
